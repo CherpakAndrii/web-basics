54c716392a32e26bb1bc2639acb139112b4f0c31
Andrew <a.cherpak-2026@kpi.ua>
2023-10-30 14:52:25 +0200

    Lab4 (#3)
    * simple chat created
    * full messenger done

---
+++ Lab4/src/index.js
@@ -0,0 +1,77 @@
+﻿const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use('/static', express.static(__dirname +'/public'));
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+});
+
+const users = {};
+const chats = {};
+
+io.on('connection', (socket) => {
+	socket.on('new user', (username) => {
+		if (username in users) {
+			socket.emit('username taken', username);
+		} else {
+			socket.emit('username available', chats);
+			socket.username = username;
+			users[username] = socket.id;
+		}
+	});
+
+	socket.on('create chat', (chatname) => {
+		if (chatname in chats) {
+			socket.emit('chatname taken', chatname);
+		} else {
+			socket.emit('chatname available', chatname);
+			chats[chatname] = {messages: [], users: []};
+			io.emit('new chat', chatname);
+		}
+	});
+
+	socket.on('join chat', (chatname) => {
+		if (socket.currentChat !== undefined){
+			chats[socket.currentChat].messages.push({type: "disconn", user: socket.username});
+			let index = chats[socket.currentChat].users.indexOf(socket.username);
+			if (index > -1){
+				chats[socket.currentChat].users.splice(index, 1);
+			}
+			io.emit('user leaved', {username: socket.username, chat: socket.currentChat});
+		}
+		socket.currentChat = chatname;
+		chats[socket.currentChat].users.push(socket.username);
+		chats[socket.currentChat].messages.push({type: "conn", user: socket.username});
+		io.emit('user joined', {username: socket.username, chat: socket.currentChat});
+		socket.emit('history', chats[socket.currentChat].messages);
+		socket.emit('users', chats[socket.currentChat].users);
+	});
+
+	socket.on('chat message', (message) => {
+		io.emit('chat message', { username: socket.username, chat: socket.currentChat, message: message });
+		chats[socket.currentChat].messages.push({type: "mess", user: socket.username, message: message});
+	});
+
+	socket.on('disconnect', () => {
+		delete users[socket.username];
+		
+		if (socket.currentChat !== undefined){
+			io.emit('user leaved', {username: socket.username, chat: socket.currentChat})
+			chats[socket.currentChat].messages.push({type: "disconn", user: socket.username});
+			let index = chats[socket.currentChat].users.indexOf(socket.username);
+			if (index > -1){
+				chats[socket.currentChat].users.splice(index, 1);
+			}
+		}
+	});
+});
+
+server.listen(3000, () => {
+	console.log('Сервер запущено на порту 3000');
+});
---
+++ Lab4/src/public/script.js
@@ -0,0 +1,178 @@
+﻿const socket = io();
+const joinForm = document.getElementById('join-form');
+const chatArea = document.getElementById('main-area');
+const chatMessages = document.getElementById('messages');
+const chatList = document.getElementById('chat-list');
+const participants = document.getElementById('participants-list');
+const currentChatTitle = document.getElementById('title');
+
+let currentChatParticipants = []
+let activePopUps = 0;
+
+function addMessage(user, message){
+	chatMessages.innerHTML = `<li><strong>${user}:</strong> ${message}</li>` + chatMessages.innerHTML;
+}
+
+function addJoinMessage(user){
+	chatMessages.innerHTML = `<li>${user} приєднався(лася) до розмови</li>` + chatMessages.innerHTML;
+}
+
+function addLeaveMessage(user){
+	chatMessages.innerHTML = `<li>${user} залишив(ла) чат</li>` + chatMessages.innerHTML;
+}
+
+function joinChat(chatLink){
+	let chatname = chatLink.innerText;
+	socket.emit('join chat', chatname);
+	currentChatTitle.innerText = chatname;
+	socket.currentChat = chatname;
+	document.getElementById('message-form').style.visibility = 'visible';
+}
+
+function addChatToList(chatname){
+	chatList.innerHTML += `<li onclick="joinChat(this)">${chatname}</li>`;
+}
+
+function showPopup(text){
+	activePopUps += 1;
+	document.getElementById('popup-text').innerText = text;
+	document.getElementById('popup').classList.add('popup-show');
+	document.getElementById('timer-bar').classList.remove('timer-running');
+	setTimeout(() => {
+		document.getElementById('timer-bar').classList.add('timer-running');
+	}, 10);
+	setTimeout(() => {
+		activePopUps -= 1;
+		if (activePopUps === 0){
+			document.getElementById('popup').classList.remove('popup-show');
+			setTimeout(() => {
+				if (activePopUps === 0)
+					document.getElementById('timer-bar').classList.remove('timer-running');
+			}, 1000);
+		}
+	}, 3500);
+}
+
+function updateParticipantsList(){
+	let users = '';
+	for (let i = 0; i < currentChatParticipants.length; i++) {
+		let usr = currentChatParticipants[i];
+		users += `<li>${usr}</li>`;
+	}
+	participants.innerHTML = users;
+}
+
+document.getElementById('log-in').addEventListener('click', () => {
+	const username = document.getElementById('username').value;
+	if (username) {
+		socket.emit('new user', username);
+		socket.username = username;
+	}
+});
+
+document.getElementById('username').addEventListener("keypress", function(event) {
+	if (event.key === "Enter") {
+		event.preventDefault();
+		document.getElementById("log-in").click();
+	}
+});
+
+document.getElementById('create').addEventListener('click', () => {
+	const chatname = document.getElementById('chatname-inp').value.toString();
+	if (chatname) {
+		socket.emit('create chat', chatname);
+	}
+});
+
+document.getElementById('chatname-inp').addEventListener("keypress", function(event) {
+	if (event.key === "Enter") {
+		event.preventDefault();
+		document.getElementById("create").click();
+	}
+});
+
+socket.on('chatname taken', (chatname) => {
+	showPopup(`Чат з назвою "${chatname}" вже існує!`);
+});
+
+socket.on('chatname available', (chatname) => {
+	document.getElementById('chatname-inp').value = '';
+	showPopup(`Новий чат "${chatname}" створено!`)
+});
+
+socket.on('username taken', (username) => {
+	showPopup(`Користувач з ім\'ям "${username}" вже існує!`);
+});
+
+socket.on('username available', (chatsList) => {
+	joinForm.style.display = 'none';
+	chatArea.style.display = 'flex';
+	for (let ch in chatsList){
+		addChatToList(ch);
+	}
+});
+
+socket.on('new chat', (chatname) => {
+	addChatToList(chatname);
+});
+
+socket.on('user joined', (user_chat) => {
+	if (user_chat.chat === socket.currentChat){
+		addJoinMessage(user_chat.username);
+		currentChatParticipants.push(user_chat.username);
+		updateParticipantsList();
+	}
+});
+
+socket.on('history', (messages) => {
+	chatMessages.innerHTML = '';
+	for (let i = 0; i < messages.length; i++) {
+		let msg = messages[i];
+		if (msg.type === "conn"){
+			addJoinMessage(msg.user);
+		}
+		else if (msg.type === "discon"){
+			addLeaveMessage(msg.user);
+		}
+		else if (msg.type === "mess"){
+			addMessage(msg.user, msg.message);
+		}
+	}
+});
+
+socket.on('users', (users) => {
+	currentChatParticipants = users;
+	updateParticipantsList();
+});
+
+socket.on('user leaved', (user_chat) => {
+	if (user_chat.chat === socket.currentChat) {
+		addLeaveMessage(user_chat.username);
+		let index = currentChatParticipants.indexOf(user_chat.username);
+		if (index > -1){
+			currentChatParticipants.splice(index, 1);
+			updateParticipantsList();
+		}
+	}
+});
+
+socket.on('chat message', (data) => {
+	if (data.chat === socket.currentChat) {
+		addMessage(data.username, data.message);
+	}
+});
+
+document.getElementById('send').addEventListener('click', () => {
+	const message = document.getElementById('message').value.toString();
+	if (message) {
+		socket.emit('chat message', message);
+		document.getElementById('message').value = '';
+	}
+});
+
+document.getElementById('message').addEventListener("keypress", function(event) {
+	if (event.key === "Enter") {
+		event.preventDefault();
+		document.getElementById("send").click();
+	}
+});
